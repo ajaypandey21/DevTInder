4f9f6d8e76914f1bc090c6b3c5c452ad92219e22
Ajay <pajay7686@gmail.com>
2025-01-14 11:43:53 +0530

    middleware handling fixed

--- src/utils/middlewares.js
+++ src/utils/middlewares.js
@@ -5,7 +5,7 @@ const userAuth = async (req, res, next) => {
   try {
     const { token } = req.cookies;
     if (!token) {
-      throw new Error("Invalid Token!");
+      return res.status(401).send("Please Login!");
     } else {
       const decodedObj = await jwt.verify(token, "JwtSecretKey1234", {
         expiresIn: "1h",
