8214e802847d170098f35b96efc6e3cc7c8b6066
Ajay <pajay7686@gmail.com>
2025-01-16 22:45:50 +0530

    socet

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,6 @@
 const socket = require("socket.io");
 const crypto = require("crypto");
+const Chat = require("../models/chat");
 const getSecretRoomId = (userId, targetUserId) => {
   return crypto
     .createHash("sha256")
@@ -14,16 +15,27 @@ const initializeSocket = (server) => {
   });
   io.on("connection", (socket) => {
     //handle events
-    socket.on("joinChat", ({ firstName, lastName, userId, targetUserId }) => {
+    socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
       const roomId = getSecretRoomId(userId, targetUserId);
       console.log(firstName + " Joined   " + roomId);
       socket.join(roomId);
     });
     socket.on(
       "sendMessage",
-      ({ firstName, lastName, userId, targetUserId, text }) => {
+      async ({ firstName, lastName, userId, targetUserId, text }) => {
         const roomId = getSecretRoomId(userId, targetUserId);
         console.log(firstName, ":", text);
+        let chat = await Chat.findOne({
+          participants: { $all: [userId, targetUserId] },
+        });
+        if (!chat) {
+          chat = new Chat({
+            participants: [userId, targetUserId],
+            messages: [],
+          });
+        }
+        chat.messages.push({ senderId: userId, text });
+        await chat.save();
         io.to(roomId).emit("messageReceived", { firstName, lastName, text });
       }
     );
